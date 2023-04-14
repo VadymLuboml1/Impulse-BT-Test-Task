9bfbfd456417ebcc1a122b2d8c8c4501b1310fec
Vadym Halaziuk <galazjukvadim@gmail.com>
2023-04-14 22:03:17 +0300

    changed primary color

--- app/src/main/java/com/example/impulsebttesttask/common/theme/Color.kt
+++ app/src/main/java/com/example/impulsebttesttask/common/theme/Color.kt
@@ -3,6 +3,6 @@ package com.example.impulsebttesttask.common.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
+val PrimaryColor = Color(0xFF025968)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
