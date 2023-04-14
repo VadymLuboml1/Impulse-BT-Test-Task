258d5b1befa17189292c37b880ea0a1d37fd7f2d
Vadym Halaziuk <galazjukvadim@gmail.com>
2023-04-14 22:01:52 +0300

    added assest sources

---
+++ app/src/main/java/com/example/impulsebttesttask/common/theme/Dimens.kt
@@ -0,0 +1,14 @@
+package com.example.impulsebttesttask.common.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+
+    val gabRound = 24.dp
+    val gabHeight = 48.dp
+
+    val gabShortHorizontalPadding = 60.dp
+    val gabLongHorizontalPadding = 36.dp
+
+
+}
---
+++ app/src/main/java/com/example/impulsebttesttask/common/theme/TextSizes.kt
@@ -0,0 +1,8 @@
+package com.example.impulsebttesttask.common.theme
+
+import androidx.compose.ui.unit.sp
+
+object TextSizes {
+
+    val gabSize = 16.sp
+}
