6e8d116d7d9bd3fb12a1e40ec620263959f99309
Vadym Halaziuk <galazjukvadim@gmail.com>
2023-04-14 22:08:39 +0300

    implemented StartScreen.kt

--- app/src/main/java/com/example/impulsebttesttask/start/StartScreen.kt
+++ app/src/main/java/com/example/impulsebttesttask/start/StartScreen.kt
@@ -1,8 +1,25 @@
 package com.example.impulsebttesttask.start
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.impulsebttesttask.R
+import com.example.impulsebttesttask.common.compose.LocalNavigationProvider
+import com.example.impulsebttesttask.common.compose.background.MainBackgroundContainer
+import com.example.impulsebttesttask.common.compose.elements.gab.GabType
+import com.example.impulsebttesttask.common.compose.elements.gab.GameActionButton
+import com.example.impulsebttesttask.common.navigation.Screen
 
 @Composable
-fun StartScreen(){
+fun StartScreen() {
+    MainBackgroundContainer {
 
+        val navController = LocalNavigationProvider.current
+
+        GameActionButton(
+            text = R.string.start_screen_button,
+            modifier = Modifier.align(Alignment.Center),
+            type = GabType.SHORT,
+        ) { navController.navigate(Screen.Quiz.decorateRouteWithParam()) }
+    }
 }
