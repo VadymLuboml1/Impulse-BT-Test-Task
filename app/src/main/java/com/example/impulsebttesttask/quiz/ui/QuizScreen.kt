3e4a26cf5c10a41fd1d7188c56b051b328b02374
Vadym Halaziuk <galazjukvadim@gmail.com>
2023-04-15 12:16:13 +0300

    added view model to quiz

--- app/src/main/java/com/example/impulsebttesttask/quiz/ui/QuizScreen.kt
+++ app/src/main/java/com/example/impulsebttesttask/quiz/ui/QuizScreen.kt
@@ -1,8 +1,12 @@
 package com.example.impulsebttesttask.quiz.ui
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun QuizScreen(level: Int) {
+fun QuizScreen(
+    level: Int,
+    viewModel: QuizViewModel = viewModel()
+) {
 
 }
---
+++ app/src/main/java/com/example/impulsebttesttask/quiz/ui/QuizViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.impulsebttesttask.quiz.ui
+
+import android.view.View
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class QuizViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
